b1fae7664efde26d667f9ebf2cce2e0c2228c78f
morfishman <2023mor.p@ebin.ort.org.il>
2024-01-21 18:29:16 +0200

    Parser Object Calc_Postfix function that convert the Object array that created in the lexer stage and convert it to an postfix expression notation

--- src/Parser.py
+++ src/Parser.py
@@ -14,4 +14,43 @@ class Parser:
             raise SyntaxExpretionExeption(self.lexerment.index-1)
         self.final_expo = []
 
-
+    def calc_postfix(self):
+        """
+        :return: void, in self.final_expo puts the postfix expression.
+        :raise: ArithmeticError
+        """
+        operators_stack = []
+        for element in self.lexerment.final_expression:
+            if type(element) == float:
+                self.final_expo.append(element)
+            else:
+                if isinstance(element,Braket) and element.side == 0:
+                    operators_stack.append(element)
+                elif isinstance(element,Braket) and element.side == 1:
+                    while operators_stack != [] and not (operators_stack[len(operators_stack) - 1]).value == '(':
+                        self.final_expo.append(operators_stack.pop())
+                    if operators_stack == [] or not (operators_stack[len(operators_stack) - 1]).value == '(':
+                        raise ArithmeticError("Not Valid Use Of Brakets: missing '(' ")#make it more castume exeption :)
+                    operators_stack.pop()
+                elif operators_stack == []:
+                    operators_stack.append(element)
+                else:
+                    if (element).unary and (isinstance(element, UnaryMinus) or isinstance(element,MaxUnaryMinus)):
+                        if (operators_stack[len(operators_stack)-1]).priority <= (element).priority:
+                            operators_stack.append(element)
+                        else:
+                            while operators_stack != [] and not ((operators_stack[len(operators_stack)-1]).priority <= (element).priority):
+                                self.final_expo.append(operators_stack.pop())
+                            operators_stack.append(element)
+                    else:
+                        if (operators_stack[len(operators_stack)-1]).priority < (element).priority:
+                            operators_stack.append(element)
+                        else:
+                            while operators_stack != [] and not ((operators_stack[len(operators_stack)-1]).priority < (element).priority):
+                                self.final_expo.append(operators_stack.pop())
+                            operators_stack.append(element)
+        while operators_stack != []:
+            operator = operators_stack.pop()
+            if isinstance(operator, Braket):
+                raise ArithmeticError("Not Valid Use Of Brakets: missing ')' ")  # make it more castume exeption :)
+            self.final_expo.append(operator)
