9b6e14de1cc2be9f1e1cf60ac85309d9eae94474
morfishman <2023mor.p@ebin.ort.org.il>
2024-01-21 18:22:04 +0200

    Custome Exeption Created for syntax check

---
+++ src/Exeptions.py
@@ -0,0 +1,6 @@
+class SyntaxExpretionExeption(Exception):
+    def __init__(self,index):
+        self.index = index
+        self.message = f'bad syntax at index:{index}'
+        super().__init__(self.message)
+
